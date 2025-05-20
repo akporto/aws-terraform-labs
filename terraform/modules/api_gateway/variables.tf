5901f7efd25d0239af2da8a5f9eeded84df100bd
Ana Kellen Porto <86116339+akporto@users.noreply.github.com>
2025-05-20 10:43:52 -0300

    Merge dev into main resolvido (#22)
    * feat: adiciona a função Lambda 'funcao-dois' para inserir itens no DynamoDB
    
    * feat(lambda): implementa LambdaTres para atualização de listas de compras (#4)
    
    * feat: implement delete Lambda function (#6)
    
    * feat(lambda): implementa LambdaTres para atualização de listas de compras (#4)
    
    * docs: atualiza instruções de instalação no README
    
    * Update README.md
    
    * Update README.md
    
    * docs: atualiza instruções de instalação no README#10)
    
    * Remove pastas .idea e .vscode do controle de versão
    
    * docs: Atualiza README.md (#15)
    
    * feat: refatora projeto migrando de Java para Python
    
    * Removendo arquivos .zip do repositório
    
    * Atualiza .gitignore para ignorar arquivos zip
    
    * Atualiza .gitignore para ignorar .zip
    
    * Refatora estrutura da API Gateway para formato modular (#20)

---
+++ terraform/modules/api_gateway/variables.tf
@@ -0,0 +1,29 @@
+variable "project_name" {
+  description = "Nome do projeto"
+  type        = string
+}
+
+variable "environment" {
+  description = "Ambiente (ex: dev, prod, staging)"
+  type        = string
+}
+
+variable "lambda_function_post_arn" {
+  description = "ARN da Lambda para POST"
+  type        = string
+}
+
+variable "lambda_function_put_arn" {
+  description = "ARN da Lambda para PUT"
+  type        = string
+}
+
+variable "lambda_function_delete_arn" {
+  description = "ARN da Lambda para DELETE"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "Região da AWS (ex: sa-east-1)"
+  type        = string
+}
